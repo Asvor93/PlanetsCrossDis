1716ae796e1fa844d2ff5091b37447acf459cf3e
Asvør <asvor_rasmussen@hotmail.com>
2019-10-07 10:44:45 +0200

    added dependencies

--- PlanetsCrossDis.RestApi/Program.cs
+++ PlanetsCrossDis.RestApi/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Planets.Cross.RestApi
+namespace PlanetsCrossDis.RestApi
 {
     public class Program
     {

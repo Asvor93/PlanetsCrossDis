fad6bdf961f7131d5362cebeda96774f24bc5853
Asvør <asvor_rasmussen@hotmail.com>
2019-10-07 10:32:04 +0200

    classes

--- Planets.Core.Entity/Planet.cs
+++ Planets.Core.Entity/Planet.cs
@@ -2,6 +2,13 @@
 {
     public class Planet
     {
-        
+        public int Id { get; set; }
+        public string PlanetName { get; set; }
+        public double Price { get; set; }
+        public int Population { get; set; }
+        public string Gravity { get; set; }
+        public string Size { get; set; }
+        public string MainResource { get; set; }
+
     }
 }

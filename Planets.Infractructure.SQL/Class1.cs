fcfe86268f4dfc1f975e5f1b69424186c2c7128b
mapoul <mpoulsen1989@gmail.com>
2019-10-07 10:32:33 +0200

    FakeDBClass

--- Planets.Infractructure.SQL/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Planets.Infractructure.SQL
-{
-    public class Class1
-    {
-    }
-}
---
+++ Planets.Infractructure.SQL/FakeDB.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Planets.Infrastructure.SQL
+{
+    class FakeDB
+    {
+    }
+}

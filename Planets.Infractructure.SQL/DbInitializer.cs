65f26ccb97c2eb5e4fd147b04cff90e287323180
mapoul <mpoulsen1989@gmail.com>
2019-10-07 11:43:38 +0200

    DummyData

---
+++ Planets.Infractructure.SQL/DbInitializer.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Planets.Infrastructure.SQL
+{
+    public class FakeDB
+    {
+        int id = 1;
+        List<Moon> _moons = new List<Moon>();
+        List<Planet> _planets = new List<Planet>();
+        List<Star> _stars = new List<Star>();
+
+        public void InitData()
+        {
+            var moon1 = new Moon()
+            {
+               
+            };
+
+        }
+
+
+    }
+}

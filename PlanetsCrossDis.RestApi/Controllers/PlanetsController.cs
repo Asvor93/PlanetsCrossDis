9e8221d321b99b0319a058e6ef786ece9fb1f352
mapoul <mpoulsen1989@gmail.com>
2019-10-08 14:15:36 +0200

    controller crud

--- PlanetsCrossDis.RestApi/Controllers/PlanetsController.cs
+++ PlanetsCrossDis.RestApi/Controllers/PlanetsController.cs
@@ -30,5 +30,61 @@ namespace PlanetsCrossDis.RestApi.Controllers
             }
             
         }
+        // GET api/planets/5
+        [HttpGet("{id}")]
+        public ActionResult<Universe> Get(int id)
+        {
+            try
+            {
+                return Ok(_planetService.GetPlanets());
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        // POST api/planets
+        [HttpPost]
+        public ActionResult<Universe> Post([FromBody] Universe universeToAdd)
+        {
+            try
+            {
+                return Ok(_planetService.AddPlanet(universeToAdd));
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        // PUT api/planets/5
+        [HttpPut("{id}")]
+        public ActionResult<Universe> Put(int id, [FromBody] Universe universeToUpdate)
+        {
+            try
+            {
+                return Ok(_planetService.UpdatePlanet(universeToUpdate);
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        // DELETE api/planets/5
+        [HttpDelete]
+        public ActionResult<Universe> Delete([FromQuery] int id)
+        {
+            try
+            {
+                return Ok(_planetService.DeletePlanet(id > 0 ? new Universe() { Id = id} : null));
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
     }
 }

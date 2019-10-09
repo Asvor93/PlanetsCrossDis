37054e81cf2374670be20dbe0cab2b4a2b6c7898
Asvør <asvor_rasmussen@hotmail.com>
2019-10-09 10:29:56 +0200

    Update OrdersController.cs

--- PlanetsCrossDis.RestApi/Controllers/OrdersController.cs
+++ PlanetsCrossDis.RestApi/Controllers/OrdersController.cs
@@ -4,12 +4,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Planets.Core.ApplicationService;
+using Planets.Core.Entity;
 
 namespace PlanetsCrossDis.RestApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+
     public class OrdersController : ControllerBase
     {
+        private readonly IOrderService _orderService;
+
+        public OrdersController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        //Get api/orders
+        [HttpGet]
+        public ActionResult<Order> Get()
+        {
+            try
+            {
+                return Ok(_orderService.ReadAllOrders());
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException();
+            }
+        }
     }
 }
